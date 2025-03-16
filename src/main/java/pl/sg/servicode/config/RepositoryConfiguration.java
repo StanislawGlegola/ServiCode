3ef71931c5d444c4060784525848fb6336f17c1b
StanislawGlegola <sta.glegola@gmail.com>
2025-03-17 00:47:04 +0100

    Update repository configuration

--- src/main/java/pl/sg/servicode/config/RepositoryConfiguration.java
+++ src/main/java/pl/sg/servicode/config/RepositoryConfiguration.java
@@ -18,14 +18,42 @@ public class RepositoryConfiguration {
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        String dbHost = env.getProperty("DB_HOST");
-        String dbPort = env.getProperty("DB_PORT");
-        String dbSid = env.getProperty("DB_SID");
-        String url = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbSid + "?allowPublicKeyRetrieval=true&useSSL=false";
-        dataSource.setUrl(url);
-        dataSource.setUsername(env.getProperty("DB_USERNAME"));
-        dataSource.setPassword(env.getProperty("DB_PASSWORD"));
+        
+        // Sprawdź czy jesteśmy w Railway
+        String railwayHost = env.getProperty("MYSQLHOST");
+        String railwayPort = env.getProperty("MYSQLPORT");
+        String railwayDatabase = env.getProperty("MYSQLDATABASE");
+        String railwayUser = env.getProperty("MYSQLUSER");
+        String railwayPassword = env.getProperty("MYSQLPASSWORD");
+        
+        if (railwayHost != null) {
+            // Użyj konfiguracji Railway
+            String url = String.format("jdbc:mysql://%s:%s/%s?allowPublicKeyRetrieval=true&useSSL=false",
+                    railwayHost, railwayPort, railwayDatabase);
+            dataSource.setUrl(url);
+            dataSource.setUsername(railwayUser);
+            dataSource.setPassword(railwayPassword);
+        } else {
+            // Użyj lokalnej konfiguracji
+            String dbHost = env.getProperty("DB_HOST");
+            String dbPort = env.getProperty("DB_PORT");
+            String dbSid = env.getProperty("DB_SID");
+            String url = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbSid 
+                        + "?allowPublicKeyRetrieval=true&useSSL=false";
+            dataSource.setUrl(url);
+            dataSource.setUsername(env.getProperty("DB_USERNAME"));
+            dataSource.setPassword(env.getProperty("DB_PASSWORD"));
+        }
+        
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        
+        // Dodaj konfigurację puli połączeń
+        dataSource.setInitialSize(1);
+        dataSource.setMaxTotal(3);
+        dataSource.setMaxWaitMillis(10000);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setValidationQuery("SELECT 1");
+        
         return dataSource;
     }
 
